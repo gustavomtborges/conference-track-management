8cae0fcad105b5ed5a10f2b1deba9d74156a56e0
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-03-13 18:56:00 -0300

    refactor talks controller

--- src/controllers/talks.js
+++ src/controllers/talks.js
@@ -1,29 +1,59 @@
 import { MORNING_SESSION_DURATION, INITIAL_TIME } from '../configs/constants';
 
-export default function setTimeForTalks(tracks) {
-  const updatedTracks = tracks.map((track) => {
-    let trackDuration = track.talks[0].min;
-    let talkDuration = 0;
+function setTimeForMorningTalks(talks, trackDuration, morningTalkDuration) {
+  return talks.map((talk) => {
+    if (trackDuration <= MORNING_SESSION_DURATION) {
+      INITIAL_TIME.setHours(9, 0, 0);
+      INITIAL_TIME.setMinutes(morningTalkDuration);
+      talk.time = INITIAL_TIME.toLocaleTimeString([],
+        { hour: '2-digit', minute: '2-digit' });
+      morningTalkDuration += talk.min;
+      trackDuration += talk.min;
+    }
+    return talk;
+  }).filter(talk => talk.time);
+}
 
-    const morningTalks = track.talks.map((talk) => {
-      if (trackDuration <= MORNING_SESSION_DURATION) {
-        INITIAL_TIME.setHours(9, 0, 0);
-        INITIAL_TIME.setMinutes(talkDuration);
-        talk.time = INITIAL_TIME.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' });
-        talkDuration += talk.min;
-        trackDuration += talk.min;
-      }
-      return talk;
-    }).filter(talk => talk.time);
+function setTimeForAfternoonTalks(lastTalks, trackDuration, afternoonTalkDuration) {
+  return lastTalks.map((talk) => {
+    INITIAL_TIME.setHours(13, 0, 0);
+    INITIAL_TIME.setMinutes(afternoonTalkDuration);
+    talk.time = INITIAL_TIME.toLocaleTimeString([],
+    { hour: '2-digit', minute: '2-digit' });
+    afternoonTalkDuration += talk.min;
+    trackDuration += talk.min;
+
+    return talk;
+  });
+}
+
+
+export default function setTimeForTalks(tracks) {
+  return tracks.map((track) => {
+    const trackDuration = track.talks[0].min;
+    const morningTalkDuration = 0;
+    const afternoonTalkDuration = 0;
+
+    // set time and return morning talks
+    const morningTalks =
+      setTimeForMorningTalks(track.talks, trackDuration, morningTalkDuration);
+
+    // set time for Lunch
     morningTalks.push({ name: 'Lunch', time: '12:00 PM' });
 
-    const afternoonTalks = track.talks.filter(talk => !talk.time);
-    console.log(morningTalks);
-    console.log(afternoonTalks);
+    // get talks without time yet
+    const lastTalks = track.talks.filter(talk => !talk.time);
+
+    // set time and return afternoon talks
+    const afternoonTalks =
+      setTimeForAfternoonTalks(lastTalks, trackDuration, afternoonTalkDuration);
+
+    // set time for Networking Event
+    afternoonTalks.push({ name: 'Networking Event', time: '5:00 PM' });
+
+    // set morning and afternoon talks in your track
+    track.talks = morningTalks.concat(afternoonTalks);
 
     return track;
   });
-
-  console.log(updatedTracks[0]);
-  return updatedTracks;
 }
