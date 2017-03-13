622332bae039f9498c9cc1ed8282b35ad96ece9e
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-03-13 11:16:27 -0300

    add new pipeline to execute app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import {
-  readContentFile, contentFileToArray, printOutput,
-} from './utils/file-input';
-import calcNumberOfTracks from './controllers/tracks';
+import { readContentFile, contentFileToArray, formatOutput, printOutput } from './utils/file-input';
+import { splitMinuteAndNameFromTalks, sortByLargestToSmallestTalk } from './utils/array';
+import { calcNumberOfTracks, pickUpTalksForTrack } from './controllers/tracks';
 
 export default () => {
+  if (!process.argv[2]) {
+    process.stdout.write('You need to pass your-input.txt as argument!\n\nTry $ npm start -- input.txt\n\n');
+    return;
+  }
   readContentFile()
     .then(fileContent => contentFileToArray(fileContent))
-    .then(arrayOfTalks => calcNumberOfTracks(arrayOfTalks))
-    .then(numberOfTracks => printOutput(numberOfTracks));
+    .then(arrayOfTalks => splitMinuteAndNameFromTalks(arrayOfTalks))
+    .then(talks => sortByLargestToSmallestTalk(talks))
+    .then(sortedTalks => calcNumberOfTracks(sortedTalks))
+    .then(data => pickUpTalksForTrack(data))
+    .then(result => formatOutput(result))
+    .then(formatedResult => printOutput(formatedResult))
+    .catch(err => process.stdout.write(err));
 };
